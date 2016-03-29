5aec19cc3b1f4650828cf8375bd7ab7c67eee13f
heyjoeb <heyjoeb@hotmail.com>
2016-03-28 21:52:23 -0500

    Added layout for process and initial layput for blog feed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,21 @@
+/* global $ */
+'use strict';
 $(document).ready(function(){
-  /* jshint strict: false */
-  /*jshint camelcase: false */
-
-  
-  // sliders
-  $('.main-slider').bxSlider({
-  	controls: false
-  });
-
-  $('.stories-slider').bxSlider({
-	pagerCustom: '#stories-slider-pager'
-  });
-
-  // responsive video embed
-  $(".responsive-video-embed").fitVids();
-  
+	/* jshint strict: false */
+	/*jshint camelcase: false */
+	// sliders
+	$('.main-slider').bxSlider({
+		controls: false
+	});
+	$('.stories-slider').bxSlider({
+		pagerCustom: '#stories-slider-pager'
+	});
+	$('.process-slider').bxSlider({
+		controls: false,
+		mode: 'fade',
+		pagerCustom: '#process-slider-pager'
+	});
+	// responsive video embed
+	$('.responsive-video-embed').fitVids();
 });
+
