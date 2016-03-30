c8c6b2ebffb97ffc006cf5f3613da47f885ee575
heyjoeb <heyjoeb@hotmail.com>
2016-03-30 13:01:51 -0500

    Added styles for navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,5 +17,13 @@ $(document).ready(function(){
 	});
 	// responsive video embed
 	$('.responsive-video-embed').fitVids();
+	// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  	$('.dropdown').on('show.bs.dropdown', function(e){
+    	$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  	});
+  	// ADD SLIDEUP ANIMATION TO DROPDOWN //
+  	$('.dropdown').on('hide.bs.dropdown', function(e){
+    	$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  	});
 });
 
